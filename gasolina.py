27ea0135e61000a01f021ae31ced1986c5e6476c
ralfeteo <ralfeteo@gmail.com>
2023-04-24 03:17:29 +0000

    Código e Gráfico png

--- gasolina.py
+++ gasolina.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 plt.figure(figsize=(10,8))
 
 with sns.axes_style('whitegrid'):
-  plt.title('Preço da gasolina,SP July.2021', fontsize=18)
+  plt.title('Preço da gasolina,SP Julho.2021', fontsize=18)
   grafico = sns.lineplot(data=gasolina_df, x="dia", y="venda")
-  grafico.set(xlabel='Dia', ylabel='Valor da venda' )
+  grafico.set(xlabel='Dia', ylabel='Preço da venda' )
   grafico.get_figure().savefig("Preço médio da gasolina.png")
