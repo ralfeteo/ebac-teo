d8de96698652fe64f31159c4592d0ac77737c3ec
ralfeteo <ralfeteo@gmail.com>
2023-04-24 01:56:00 +0000

    Código e Gráfico png

--- gasolina.py
+++ gasolina.py
@@ -1,6 +1,3 @@
-import pandas as pd
-gasolina_df= pd.read_csv('gasolina.csv')
-
 # código de geração do gráfico gasolina.py
 
 import pandas as pd
@@ -9,7 +6,7 @@ import matplotlib.pyplot as plt
 plt.figure(figsize=(10,8))
 
 with sns.axes_style('whitegrid'):
-  plt.title('Preço médio da gasolina SP, Julho.2021', fontsize=18)
+  plt.title('Preço da gasolina,SP Jul.2021', fontsize=18)
   grafico = sns.lineplot(data=gasolina_df, x="dia", y="venda")
-  grafico.set(xlabel='Dia', ylabel='Preço de venda' )
+  grafico.set(xlabel='Dia', ylabel='Valor da venda' )
   grafico.get_figure().savefig("Preço médio da gasolina.png")
